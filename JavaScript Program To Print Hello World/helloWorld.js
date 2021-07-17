0813d6bf1772d4bba3d0b31d78f022c88632bef3
shailesh-chaudhari <shailesh9423@gmail.com>
2021-07-18 01:58:40 +0530

    first commit

---
+++ JavaScript Program To Print Hello World/helloWorld.js
@@ -0,0 +1,9 @@
+/*Question:-
+ JavaScript Program To Print Hello World
+*/
+
+//Answer:-
+console.log("Hello World !"); // Using console.log()
+alert("Hello World !"); //Using alert()
+document.write("Hello World !");//Using document.write()
+prompt("Hello World !");//using prompt()
