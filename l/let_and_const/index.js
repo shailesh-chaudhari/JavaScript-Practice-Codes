c2ac1c1ecd6e57b27a29830f284cd79dba3d5c8c
shailesh chaudhari <59895316+shailesh-chaudhari@users.noreply.github.com>
2024-09-12 02:10:12 +0530

    added JavaScript code

---
+++ l/let_and_const/index.js
@@ -0,0 +1,26 @@
+let num1 = 10
+
+console.log(num1)
+
+let boyName = 'RAM'
+let isOldEnough = false
+let marks = [10, 34, 56, 87, 65]
+let bookName = {
+    title: "half Girlfriend",
+    description: "Read the book and watch movie"
+}
+
+console.log(boyName, isOldEnough, marks, bookName)
+
+const num2 = 20
+//num2 = 40 //assignment to constant variable error 
+console.log(num2)
+
+
+num3 = 40
+console.log(num3)
+var num3 // this concept is called hoisting we can access num 3 before initialization.  let and const not allowed hoisting 
+
+num4 = 40
+console.log(num4)
+//let num4 // error Reference error cannot access num4 before initialization. let and const not allowed hoisting 
